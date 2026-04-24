af6d597b4c73e6747a7152a4c76c0f9ea4ade9b7
jonibekballasov64-sketch <jonibekballasov64@gmail.com>
2026-04-24 12:57:59 +0500

    Update comments and add startup message for bot

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from bot.config import TOKEN
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-# handlerlarni ulaymiz
+# HANDLERLARNI ULAYMIZ
 from bot.handlers import start, subscription, menu
 
 if __name__ == "__main__":
+    print("Bot ishga tushdi...")
     executor.start_polling(dp, skip_updates=True)
