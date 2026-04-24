9246155ea6435f760c2d27fd1019fe9d03faf8e1
jonibekballasov64-sketch <jonibekballasov64@gmail.com>
2026-04-24 15:04:51 +0500

    Add test creation functionality for admin users

--- bot/handlers/test_create.py
+++ bot/handlers/test_create.py
@@ -1,9 +1,15 @@
+import json
 from aiogram import types
 from main import dp
 from bot.config import ADMIN_ID
 
+# vaqtinchalik
+TESTS = {}
+
 WEBAPP_CREATE = "https://YOUR-APP.up.railway.app/create"
 
+
+# 🔹 tugma
 @dp.message_handler(text="✏️ Test yaratish")
 async def create_test(message: types.Message):
     if message.from_user.id != ADMIN_ID:
@@ -11,8 +17,33 @@ async def create_test(message: types.Message):
 
     kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
     kb.add(types.KeyboardButton(
-        text="📝 Forma ochish",
+        text="📝 Test yaratish",
         web_app=types.WebAppInfo(url=WEBAPP_CREATE)
     ))
 
     await message.answer("Test yaratish uchun bosing 👇", reply_markup=kb)
+
+
+# 🔹 webapp data
+@dp.message_handler(content_types=types.ContentType.WEB_APP_DATA)
+async def create_handler(message: types.Message):
+
+    if message.from_user.id != ADMIN_ID:
+        return
+
+    data = json.loads(message.web_app_data.data)
+
+    code = data.get("code")
+    test = data.get("test")
+
+    TESTS[code] = test
+
+    text = f"""Assalomu alaykum. Berilgan testni ishlang.
+
+🔑 Test kodi: {code}
+
+👉 @Olimovna_Rashbot orqali ishlang
+📝 45-savol esse o‘quvchi tomonidan kiritiladi"""
+
+    await message.answer("✅ Test yaratildi!")
+    await message.answer(text)
